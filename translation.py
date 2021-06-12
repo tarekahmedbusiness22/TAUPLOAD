11f5b33a97e347c0485c684bd1e5a245ee9e2e24
tarekahmedbusiness22 <81440458+tarekahmedbusiness22@users.noreply.github.com>
2021-06-12 17:13:37 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,7 +6,7 @@ This is a Telegram URL Upload Bot!
 
 /help for more details..
 
-Support Group : @TaRe8A7MeD
+Support Bot : @TaRe8A7MeD
 © @SpEcHlDe , @TaRe8A7MeD"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
